feaf92a437b7cee902bc4c159d35b48d54c22be2
luisa.xiao <luisa.xiao@bamboonetworks.com>
2020-04-17 14:13:17 +0800

    update

--- src/js/currie.js
+++ src/js/currie.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2019-11-12 15:54:26
+ * @LastEditTime: 2020-04-17 11:28:54
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \fluter_appe:\workspace\My projects\serious-review\src\js\currie.js
+ */
 /**
  * @description: 函数柯里化
  * @author: Luisa.Xiao
@@ -10,14 +18,14 @@
  */
 export default function curry(fn, ...arg) {
   let all = arg || [],
-      _len = fn.length;
-  return (...rest) =>{
+    _len = fn.length; // fn参数的个数
+  return (...rest) => {
     let _arg = all.splice(0); //拷贝新的all，避免改动公有的all属性，导致多次调用_args.length出错
     _arg.push(...rest);
     if (_arg.length < _len) {
-      return curry.call(this,fn, ..._arg)
+      return curry.call(this, fn, ..._arg)
     } else {
-      return fn.apply(this,_arg)
+      return fn.apply(this, _arg)
     }
   }
 }
--- src/js/throttle.js
+++ src/js/throttle.js
@@ -1,18 +1,27 @@
+/*
+ * @Author: your name
+ * @Date: 2019-11-08 15:56:59
+ * @LastEditTime: 2020-04-17 11:59:24
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \fluter_appe:\workspace\My projects\serious-review\src\js\throttle.js
+ */
 /**
- * 
+ * 规定在一个单位时间内，只能触发一次函数。如果这个单位时间内触发多次函数，只有一次生效，
+ * 打个比喻：就行玩LoL放大招，放完大招需要冷却一段时间才能继续放
  */
 export default function throttle(func, wait, options) {
   var context, args, result;
   var timeout = null;
   var previous = 0;
   if (!options) options = {};
-  var later = function() {
+  var later = function () {
     previous = options.leading === false ? 0 : new Date().getTime();
     timeout = null;
     result = func.apply(context, args);
     if (!timeout) context = args = null;
   };
-  return function() {
+  return function () {
     let now = new Date().getTime();
     if (!previous && options.leading === false) previous = now;
     // 计算剩余时间
